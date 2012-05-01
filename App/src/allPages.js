8153acd48364a43582ad60fe6283a8e3bae24257
Chairface @OSX <admin@chairface.org>
2012-04-30 21:59:43 -0400

    layout for livelinks support

---
+++ App/src/allPages.js
@@ -0,0 +1,26 @@
+var allPages = {
+    float_userbar: function(){
+        document.getElementsByClassName('menubar')[0].style.position = "fixed";
+        document.getElementsByClassName('menubar')[0].style.width = "99%";
+        document.getElementsByClassName('menubar')[0].style.marginTop = "-10px";
+        document.getElementsByTagName('h1')[0].style.paddingTop = '40px';
+        document.getElementsByClassName('userbar')[0].style.position = "fixed";
+        document.getElementsByClassName('userbar')[0].style.borderBottomLeftRadius = '5px';
+        document.getElementsByClassName('userbar')[0].style.borderBottomRightRadius = '5px';
+        document.getElementsByClassName('userbar')[0].style.width = "99%";
+        document.getElementsByClassName('userbar')[0].style.top = "33px";
+        document.getElementsByClassName('menubar')[0].style.marginRight = "20px";
+    }
+}
+    
+chrome.extension.sendRequest({need:"config"}, function(response){
+    for(var i in allPages){
+            if(response.data[i]){
+                try{
+                    allPages[i]();
+                }catch(err){
+                    console.log("error in " + i + ":", err);
+                }
+            }
+    }
+});
