fb4308b626a7662f8fdf27868ec246b75d4c626b
Chairface @OSX <admin@chairface.org>
2012-05-02 22:10:59 -0400

    usernotes and some batch uploader stuff

--- App/src/allBg.js
+++ App/src/allBg.js
@@ -1,5 +1,5 @@
 var allBg = {
-    activeListeners: { "force_https": false, "batch_upload": false},
+    activeListeners: { "force_https": false, "batch_uploader": false},
     init_listener: function(cfg){
         if(cfg.force_https){
             allBg.activeListeners.force_https = true;
@@ -12,21 +12,29 @@ var allBg = {
         }
 
         if(cfg.batch_uploader){
-            chrome.webRequest.onBeforeSendHeaders.addListener(function(dest){
-                var headers = dest.requestHeaders;
-                var response = {};
-                for(var i in headers){
-                    if(headers[i].name == "Referer"){
-                        headers[i].value = "http://u.endoftheinter.net/u.php";
-                        break;
-                    }
-                }
-                response.requestHeaders = headers;
-                return response;        
-            });
+            chrome.webRequest.onBeforeSendHeaders.addListener(allBg.handle_batch_uploader, {"urls":["http://u.endoftheinter.net/*", "https://u.endoftheinter.net/*"]}, ['blocking', 'requestHeaders']);
+            allBg.activeListeners.batch_uploader = true;
+        }
+        
+        if(!cfg.batch_uploader && allBg.activeListeners.batch_uploader){
+            chrome.webRequest.onBeforeSendHeaders.removeListener(allBg.handle_batch_uploader);
+            allBg.activeListeners.batch_uploader = false;
         }
     },
+    handle_batch_uploader: function(dest){
+        var headers = dest.requestHeaders;
+        var response = {};
+        console.log(headers);
+        for(var i in headers){
+            if(headers[i].name == "Referer"){
+                headers[i].value = "http://u.endoftheinter.net/u.php";
+                break;
+            }
+        }
+        response.requestHeaders = headers;
+        return response;
+    },
     handle_redirect: function(dest){
-        return { redirectUrl: dest.url.replace(/^http/i, "https")}
+        return { redirectUrl: dest.url.replace(/^http/i, "https")};
     }
 }
--- App/src/messageList.js
+++ App/src/messageList.js
@@ -1,3 +1,4 @@
+var config = {};
 var messageList = {
     click_expand_thumbnail: function(){
         for (var j = 0; j < document.getElementsByClassName('img').length; j++){
@@ -8,6 +9,28 @@ var messageList = {
     drag_resize_image: function(){
         //console.log(document.getElementsByClassName('img'));
     },
+    user_notes: function(){
+        if (!config.usernote_notes) config.usernote_notes = {};
+        document.addEventListener('click', function(tgt){
+            if(tgt.target.id == 'notebook'){
+                messageListHelper.openNote(tgt.target);
+            }
+        });
+        messageListHelper.addNotebox(document.getElementsByClassName('message-top'));
+    },        
+    batch_uploader: function(){
+        console.log('starting batch uploader');
+        var ulBox = document.createElement('input');
+        ulBox.type = 'file';
+        ulBox.multiple = true;
+        ulBox.value = "Batch Upload";
+        ulBox.name = "batch_uploads";
+        var ulButton = document.createElement('input');
+        ulButton.type = "button";
+        ulButton.value = "Batch Upload";
+        document.getElementsByClassName('quickpost-body')[0].insertBefore(ulButton, null);
+        document.getElementsByClassName('quickpost-body')[0].insertBefore(ulBox, ulButton);
+    }
 }
 var messageListHelper = {
     expandThumbnail: function(evt){
@@ -27,21 +50,81 @@ var messageListHelper = {
             console.log(evt);
         }
     },
+    addNotebox: function(tops){
+        var top;
+		for(var i=0; top = tops[i]; i++){
+			var notebook = document.createElement('a');
+			notebook.id = 'notebook';
+			top.innerHTML += " | ";
+			var tempID = top.getElementsByTagName('a')[0].href.match(/user=(\d+)$/i)[1];
+			notebook.innerHTML = (config.usernote_notes[tempID] != undefined && config.usernote_notes[tempID] != '') ? 'Notes*' : 'Notes';
+			notebook.href = "##note" + tempID;
+			top.appendChild(notebook);
+		}
+    },
+    openNote: function(el){
+        var userID = el.href.match(/note(\d+)$/i)[1];
+		if(document.getElementById("notepage")){
+			var pg = document.getElementById('notepage');
+			userID = pg.parentNode.getElementsByTagName('a')[0].href.match(/user=(\d+)$/i)[1];
+			config.usernote_notes[userID] = pg.value;
+			pg.parentNode.removeChild(pg);
+            messageListHelper.saveNotes();
+		}else{
+			var note = config.usernote_notes[userID];
+			page = document.createElement('textarea');
+			page.id='notepage';
+			page.value = (note == undefined) ? "" : note;
+			page.style.width="100%";
+			page.style.opacity = '.6';
+			el.parentNode.appendChild(page);
+		}
+	},
+    saveNotes: function(){
+        chrome.extension.sendRequest({need:"save", name:"usernote_notes", data:config.usernote_notes}, function(rsp){
+            console.log(rsp);
+        });
+    },
     init: function(){
         chrome.extension.sendRequest({need:"config"}, function(conf){
+            config = conf.data;
             for(var i in messageList){
-                if(conf.data[i]){
-                    try{
+                if(config[i]){
+                    //try{
                         messageList[i]();
-                    }catch(err){
-                        console.log("error in " + i + ":", err);
-                    }
+                    //}catch(err){
+                    //    console.log("error in " + i + ":", err);
+                    //}
                 }
             }
         });
     },
     livelinks: function(evt){
-        if (evt.target.getElementsByClassName("message-top")[0]) messageListHelper.init();
+        if (!evt.target.getElementsByClassName) return;
+        if (evt.target.getElementsByClassName("message-top")[0]){
+            for(var i in messageListLivelinks){
+                if(config[i]){
+                    //try{
+                        messageListLivelinks[i](evt.target);
+                    //}catch(err){
+                    //    console.log("error in livelinks " + i + ":", err);
+                    //}
+                }
+            }
+        }
+    }
+}
+var messageListLivelinks = {
+    click_expand_thumbnail: function(el){
+        if(el.getElementsByClassName('img')){
+            for (var j = 0; j < el.getElementsByClassName('img').length; j++){
+                el.getElementsByClassName('img')[j].addEventListener("click", messageListHelper.expandThumbnail);
+                el.getElementsByClassName('img')[j].getElementsByTagName('a')[0].removeAttribute('href');
+            }
+        }
+    },
+    user_notes: function(el){
+        messageListHelper.addNotebox(el.getElementsByClassName('message-top'));
     }
 }
 messageListHelper.init();
