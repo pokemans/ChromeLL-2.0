71316c8dfe2ef71a694a353d9bc4ab6f6d53af9e
Chairface @OSX <admin@chairface.org>
2012-05-08 21:00:43 -0400

    lots of additions

--- App/src/messageList.js
+++ App/src/messageList.js
@@ -39,6 +39,37 @@ var messageList = {
             }
         }
     },
+    imagemap_on_infobar: function(){
+        function getUrlVars(urlz){
+            var vars = [], hash;
+            var hashes = urlz.slice(urlz.indexOf('?') + 1).split('&');
+            for(var i = 0; i < hashes.length; i++){
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+                if (hash[1]!=null && hash[1].indexOf("#")>=0){
+                    vars[hash[0]]=hash[1].slice(0,hash[1].indexOf("#"));
+                }
+            }
+            return vars;
+        }
+
+        var divs=document.getElementsByClassName("infobar")[0];
+        var get=getUrlVars(window.location.href);
+        var page=location.pathname;
+
+        if (page=="/imagemap.php"&&get["topic"]!=undefined){
+            var as2=divs.getElementsByTagName("a");
+            for (var j=0; j<as2.length; j++){
+                if (as2[j].href.indexOf("imagemap.php?")>0){
+                    as2[j].href=as2[j].href+"&board="+get["board"];
+                }
+            }
+            divs.innerHTML=divs.innerHTML+" | <a href='/showmessages.php?board="+get["board"]+"&topic="+get["topic"]+"' title='Back to Topic'>Back to Topic</a>";
+        }else if (page=="/showmessages.php"){
+            divs.innerHTML=divs.innerHTML+" | <a href='/imagemap.php?board="+get["board"]+"&topic="+get["topic"]+"' title='Imagemap'>Imagemap</a>";
+        }
+    },
     batch_uploader: function(){
         var ulBox = document.createElement('input');
         ulBox.type = 'file';
@@ -75,6 +106,137 @@ var messageList = {
             styleElement.appendChild(document.createTextNode(cssCode));
         }
     	document.getElementsByTagName("head")[0].appendChild(styleElement);
+    },
+    resize_imgs: function(){
+        for(var i = 0; document.getElementsByTagName('img')[i]; i++){
+            messageListHelper.resizeImg(document.getElementsByTagName('img')[i]);
+        }
+    },
+    loadquotes: function(){
+        function getElementsByClass(searchClass,node,tag) {
+            var classElements = new Array();
+            if (node == null)
+                node = document;
+            if ( tag == null)
+                tag = '*';
+            var els = node.getElementsByTagName(tag);
+            var elsLen = els.length;
+            for (var i = 0, j = 0; i < elsLen; i++) {
+                if (els[i].className == searchClass) {
+                    classElements[j] = els[i];
+                    j++;
+                }
+            }
+            return classElements;
+        }
+
+        function imagecount() {
+            var imgs = document.getElementsByTagName('img').length;
+            return imgs;
+        }
+
+        if (document.location.href.indexOf("https") == -1) {
+            var url = "http";
+        } else {
+            var url = "https";
+        }
+        
+        function coolCursor() {
+            this.style.cursor = 'pointer';
+        }
+
+        function processPage(XML, element) {
+            var newPage = document.createElement("div");
+            newPage.innerHTML = XML;
+            var newmessage = getElementsByClass('message', newPage, null)[0];
+            var scripttags = newmessage.getElementsByTagName('script');
+            for (var i = 0; i < scripttags.length; i++) {
+                var jsSource = scripttags[i].innerHTML.replace(/onDOMContentLoaded\(function\(\)\{new ImageLoader\(\$\("u0_1"\), "\\\/\\\//gi, '').replace(/\\/gi, '').replace(/\)\}\)/gi, '').split(',');
+                var replacement = new Image();
+                replacement.src = url + '://' + jsSource[0].replace(/"$/gi, '');
+                replacement.className = 'expandimagesLOL';
+                scripttags[i].parentNode.replaceChild(replacement, scripttags[i]);
+                i--;
+            }
+            if (newmessage.innerHTML.indexOf('---') != -1) {
+                var j = 0;
+                while (newmessage.childNodes[j]) {
+                    if (newmessage.childNodes[j].nodeType == 3 && newmessage.childNodes[j].nodeValue.indexOf('---') != -1) {
+                        while (newmessage.childNodes[j]) {
+                            newmessage.removeChild(newmessage.childNodes[j]);	
+                        }
+                    }
+                    j++;
+                }
+            }
+            element.parentNode.appendChild(newmessage);
+        }
+
+        function loadMessage() {	
+            var mssgurl = this.id;
+            var newSpan = document.createElement('span');
+            newSpan.innerHTML = 'Loading message...';
+            var loadingImg = new Image();
+            loadingImg.src = 'data:image/gif;base64,' +
+                'R0lGODlhEAAQAPIAAP///2Zm/9ra/o2N/mZm/6Cg/rOz/r29/iH/C05FVFNDQVBFMi4wAwEAAAAh/hpD' +
+                'cmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZ' +
+                'nAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi6' +
+                '3P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAs' +
+                'AAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKp' +
+                'ZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8D' +
+                'YlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJU' +
+                'lIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe8' +
+                '2p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAAD' +
+                'Mgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAA' +
+                'LAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsR' +
+                'kAAAOwAAAAAAAAAAAA==';
+            this.parentNode.insertBefore(newSpan, this);
+            this.parentNode.replaceChild(loadingImg, this);
+            var ajax = new XMLHttpRequest();
+            ajax.open('GET', url + '://boards.endoftheinter.net/message.php?' + mssgurl, true);
+            ajax.send(null);
+            ajax.onreadystatechange = function() {
+                if (ajax.readyState == 4) {
+                    if (ajax.status == 200) {
+                        processPage(ajax.responseText, newSpan);
+                        loadingImg.parentNode.removeChild(loadingImg);
+                        newSpan.parentNode.removeChild(newSpan);
+                    } else {
+                        alert("An error occurred loading the message. Fuck shit.");
+                    }
+                }
+            }
+        }
+
+        function findQuotes() {
+            var quotes = getElementsByClass('quoted-message', document, 'div');
+            for (var i = 0; i < quotes.length; i++) {
+                var anchors = quotes[i].getElementsByTagName('a');
+                for (var j = 0; j < anchors.length; j++) {
+                    if (anchors[j].innerHTML == '[quoted text omitted]') {
+                        anchors[j].removeAttribute('href');
+                        var parts = anchors[j].parentNode.getAttribute('msgid').split(',');
+                        var secondsplit = parts[2].split('@');
+                        anchors[j].id = 'id=' + secondsplit[0] + '&topic=' + parts[1] + '&r=' + secondsplit[1];
+                        anchors[j].addEventListener('click', loadMessage, true);
+                        anchors[j].style.textDecoration='underline';
+                        anchors[j].title = 'Click to load the omitted message';
+                        anchors[j].addEventListener('mouseover', coolCursor, true);
+                    }
+                }	
+            }
+        }
+
+        var currentMessages = 0;
+
+        function checkMssgs() {
+            var mssgs = getElementsByClass('message-container', document.getElementById('u0_1'), 'div').length;
+            if (mssgs > currentMessages) {
+                findQuotes();
+                currentMessages = mssgs;
+            }
+        }
+        var interval = window.setInterval(checkMssgs, 1000);
     },
     quickpost_tag_buttons: function(){
         var m = document.getElementsByClassName('quickpost-body')[0];
@@ -312,6 +474,17 @@ var messageListHelper = {
             console.log(rsp);
         });
     },
+    resizeImg: function(el){
+        //console.log(el.width, config.img_max_width);
+        var width = el.width;
+        if(width > config.img_max_width){
+            if(config.debug) console.log('resizing:', el);
+            el.height = (el.height / (el.width / config.img_max_width));
+            el.parentNode.style.height = el.height + 'px';
+            el.width = config.img_max_width;
+            el.parentNode.style.width = config.img_max_width + 'px';
+        }
+    },
     saveTcs: function(){
         var max = 20;
         var lowest = Infinity;
@@ -399,6 +572,10 @@ var messageListHelper = {
                     }
                 }
             }
+        }else if(evt.target.width){
+            if(config.resize_imgs){
+                messageListLivelinks.resize_imgs(evt.target.parentNode);
+            }
         }
     }
 }
@@ -417,8 +594,14 @@ var messageListLivelinks = {
         for(var f = 0; messageListHelper.ignores[f]; f++){
             if(m.getElementsByTagName('a')[0].innerHTML.toLowerCase() == messageListHelper.ignores[f]){
                 el.style.display = "none";
-                console.log('removed livelinks post by ' + messageListHelper.ignores[f]);
+                if(config.debug) console.log('removed livelinks post by ' + messageListHelper.ignores[f]);
             }
+        }
+    },
+    resize_imgs: function(el){
+        console.log('test');
+        for(var i = 0; el.getElementsByTagName('img')[i]; i++){
+            messageListHelper.resizeImg(el.getElementsByTagName('img')[i]);
         }
     },
     user_notes: function(el){
@@ -431,6 +614,19 @@ var messageListLivelinks = {
             document.title = "(" + (posts + 1) + ") " + document.title.replace(/\(\d+\) /, "");
         }else{
             document.title = "(" + posts + ") " + document.title;
+        }
+    },
+    notify_quote_post: function(el){
+        if(!el.getElementsByClassName('quoted-message')) return;
+        var not = false;
+        var msg = el.getElementsByClassName('quoted-message');
+        for(var i = 0; msg[i]; i++){
+            if(msg[i].getElementsByClassName('message-top')[0].getElementsByTagName('a')[0].innerHTML == document.getElementsByClassName('userbar')[0].getElementsByTagName('a')[0].innerHTML.replace(/ \((.*)\)$/, "")){
+                if(msg[i].parentNode.className != 'quoted-message') not = true;
+            }
+        }
+        if(not){
+            chrome.extension.sendRequest({need:"notify", title:"Quoted by " + el.getElementsByClassName('message-top')[0].getElementsByTagName('a')[0].innerHTML, message:document.title.replace(/End of the Internet - /i, '')}, function(data){ console.log(data); });
         }
     },
     highlight_tc: function(el){
@@ -455,6 +651,9 @@ var messageListLivelinks = {
             el.getElementsByClassName('message-top')[0].style.color = '#' + config.user_highlight_data[user].color;
             for(var i = 0; el.getElementsByClassName('message-top')[0].getElementsByTagName('a')[i]; i++){
                 el.getElementsByClassName('message-top')[0].getElementsByTagName('a')[i].style.color = '#' + config.user_highlight_data[user].color;
+            }
+            if(config.notify_userhl_post){
+                chrome.extension.sendRequest({need:"notify", message:document.title.replace(/End of the Internet - /i, ''), title:"Post by " + el.getElementsByClassName('message-top')[0].getElementsByTagName('a')[0].innerHTML}, function(data){ console.log(data); });
             }
         }
     }
--- App/src/topicList.js
+++ App/src/topicList.js
@@ -4,46 +4,131 @@ var topicList = {
         if(!config.ignorator) return;
         var s;
         var ignores = config.ignorator_list.split(',');
-        for(var r = 0; r < ignores.length; r++){
-            var d = 0;
-            while(ignores[r].substring(d, d + 1) == ' '){
-                d++;
-            }
-            ignores[r] = ignores[r].substring(d,ignores[r].length);
-        }
+        ignores = topicListHelper.handleCsv(ignores);
         var g = document.getElementsByTagName('tr');
         var title;
         for(var i = 1; g[i]; i++){
             if(g[i].getElementsByTagName('td')[1]){
                 title = g[i].getElementsByTagName('td')[1];
                     for(var f = 0; f < ignores.length; f++){
-                        if(title.getElementsByTagName('a').item(0).innerHTML.toLowerCase() == ignores[f]){
-                            if(config.debug) console.log('found topic to remove: \"' + g.item(i).getElementsByTagName('td').item(0).getElementsByTagName('a').item(0).innerHTML.toLowerCase() + "\" author: " + ignores[f] + " topic: " + i);
+                        if(title.getElementsByTagName('a')[0].innerHTML.toLowerCase() == ignores[f]){
+                            if(config.debug) console.log('found topic to remove: \"' + g[i].getElementsByTagName('td')[0].getElementsByTagName('a')[0].innerHTML.toLowerCase() + "\" author: " + ignores[f] + " topic: " + i);
                             title.parentNode.style.display = 'none';				
                         }
                     }
             }
         }
     },
+    ignore_keyword: function(){
+        if(config.ignore_keyword_list == "" || config.ignore_keyword_list == undefined) return;
+        var keywords;
+        var re = false;
+        try{
+            keywords = JSON.parse(config.ignore_keyword_list);
+            if(config.debug) console.log("JSON keywords");
+            re = true;
+        }catch(e){
+            keywords = config.ignore_keyword_list.split(',');
+            keywords = topicListHelper.handleCsv(keywords);
+        }
+        if(config.debug) console.log(keywords);
+        var g = topicListHelper.getTopics();
+        var title;
+        var match = false;
+        var reg;
+        for(var i = 1; g[i]; i++){
+            if(g[i].getElementsByTagName('td')[0]){
+                title = g[i].getElementsByTagName('td')[0];
+                    for(var f = 0; f < keywords.length; f++){
+                        if(re){
+                            if(keywords[f].substring(0, 1) == '/'){
+                                reg = new RegExp(keywords[f].substring(1, keywords[f].lastIndexOf('/')), keywords[f].substring(keywords[f].lastIndexOf('/') + 1, keywords[f].length));
+                            }else{
+                                reg = keywords[f];
+                            }
+                            match = title.getElementsByTagName('a')[0].innerHTML.match(reg);
+                        }else{
+                            match = title.getElementsByTagName('a')[0].innerHTML.toLowerCase().match(keywords[f].toLowerCase());
+                        }
+                        if(match){
+                            if(config.debug) console.log('found topic to remove: \"' + g[i].getElementsByTagName('td')[0].getElementsByTagName('a')[0].innerHTML.toLowerCase() + "\" keyword: " + keywords[f] + " topic: " + i);
+                            title.parentNode.style.display = 'none';
+                            //break;
+                        }
+                    }
+            }
+        }
+    },
     page_jump_buttons: function(){
-        var trs = document.getElementsByTagName('table')[0].getElementsByTagName('tr');
-        if(document.getElementsByTagName('table')[1]){
-            trs = document.getElementsByTagName('table')[1].getElementsByTagName('tr');
-        }	
+        var trs = topicListHelper.getTopics();
         var insert;
-        var tmp;
+        var tmp, topic;
         for(var i = 1; trs[i]; i++){
             if(trs[i].getElementsByTagName('td')[0]){
                 insert = document.createElement('span');
                 insert.style.float = 'right';
                 insert.addEventListener('click', topicListHelper.jumpHandlerTopic, false);
-                tmp = trs[i].getElementsByTagName('td')[0].getElementsByTagName('a')[0].href.match(/(topic|thread)=([0-9]+)/)[2];
-                insert.innerHTML = '<a href="##' + tmp + '" id="jumpWindow">#</a> <a href="##' + tmp + '" id="jumpLast">&gt;</a>';
-                trs[i].getElementsByTagName('td')[0].insertBefore(insert, trs[i].getElementsByTagName('td')[0].getElementsByTagName('a')[0]);
+                try{
+                    topic = trs[i].getElementsByTagName('td')[0].getElementsByTagName('a');
+                    tmp = topic[0].href.match(/(topic|thread)=([0-9]+)/)[2];
+                    insert.innerHTML = '<a href="##' + tmp + '" id="jumpWindow">#</a> <a href="##' + tmp + '" id="jumpLast">&gt;</a>';
+                    trs[i].getElementsByTagName('td')[0].insertBefore(insert, null);
+                }catch(e){
+                    if(config.debug) console.log('locked topic?');
+                    topic = trs[i].getElementsByTagName('td')[0].getElementsByTagName('span')[0].getElementsByTagName('a');
+                    tmp = topic[0].href.match(/(topic|thread)=([0-9]+)/)[2];
+                    insert.innerHTML = '<a href="##' + tmp + '" id="jumpWindow">#</a> <a href="##' + tmp + '" id="jumpLast">&gt;</a>';
+                    trs[i].getElementsByTagName('td')[0].insertBefore(insert, null);
+                }
+            }
+        }
+    },
+    enable_keyword_highlight: function(){
+        var topics = topicListHelper.getTopics();
+        var title;
+        var keys = {};
+        var re = false;
+        for(var j = 0; config.keyword_highlight_data[j]; j++){
+            try{
+                keys[j] = {};
+                keys[j].match = JSON.parse(config.keyword_highlight_data[j].match);
+                keys[j].bg = config.keyword_highlight_data[j].bg;
+                keys[j].color = config.keyword_highlight_data[j].color;
+                re = true;
+            }catch(e){
+                keys[j] = {};
+                keys[j].match = config.keyword_highlight_data[j].match.split(',');
+                keys[j].match = topicListHelper.handleCsv(keys[j].match);
+                keys[j].bg = config.keyword_highlight_data[j].bg;
+                keys[j].color = config.keyword_highlight_data[j].color;
+            }
+        }
+        var reg;
+        for(var i = 1; topics[i]; i++){
+            title = topics[i].getElementsByTagName('td')[0].getElementsByTagName('a')[0].innerHTML;
+            for(var j = 0; keys[j]; j++){
+                for(var k = 0; keys[j].match[k]; k++){
+                    if(keys[j].match[k].substring(0, 1) == '/'){
+                        reg = new RegExp(keys[j].match[k].substring(1, keys[j].match[k].lastIndexOf('/')), keys[j].match[k].substring(keys[j].match[k].lastIndexOf('/') + 1, keys[j].match[k].length));
+                        match = title.match(reg);
+                    }else{
+                        reg = keys[j].match[k].toLowerCase();
+                        match = title.toLowerCase().match(reg);
+                    }
+                    if(match){
+                        topics[i].getElementsByTagName('td')[0].style.background = '#' + keys[j].bg;
+                        topics[i].getElementsByTagName('td')[0].style.color = '#' + keys[j].color;
+                        for(var m = 0; topics[i].getElementsByTagName('td')[0].getElementsByTagName('a')[m]; m++){
+                            topics[i].getElementsByTagName('td')[0].getElementsByTagName('a')[m].style.color = '#' + keys[j].color;
+                        }
+                        if(config.debug) console.log('highlight topic ' + title + ' keyword ' + reg);
+                    }
+                }
             }
         }
     },
     userhl_topiclist: function(){
+        if(!config.enable_user_highlight) return;
         var topics = topicListHelper.getTopics();
         var user;
         for(var i = 1; topics[i]; i++){
@@ -78,7 +163,17 @@ var topicListHelper = {
     },
     getTopics: function(){
         return document.getElementsByClassName('grid')[0].getElementsByTagName('tr');
-    },        
+    },
+    handleCsv: function(ignores){
+        for(var r = 0; r < ignores.length; r++){
+            var d = 0;
+            while(ignores[r].substring(d, d + 1) == ' '){
+                d++;
+            }
+            ignores[r] = ignores[r].substring(d,ignores[r].length);
+        }
+        return ignores;
+    },
     init: function(){
         chrome.extension.sendRequest({need:"config"}, function(conf){
             config = conf.data;
